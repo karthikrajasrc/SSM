d25c0d2d5641fd7af6bee1592cf79a6fe674ecd1
karthikrajasrc <karthikrajasrc@gmail.com>
2026-02-22 23:13:36 +0530

    new changes made

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,18 +5,15 @@ import image1 from "../images/image1.svg"
 import image2 from "../images/image2.svg"
 import image3 from "../images/image3.svg"
 import image4 from "../images/image4.svg"
-import logo from "../images/logo.svg"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faWhatsapp }
-    from "@fortawesome/free-brands-svg-icons";
-import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import ScrollReveal from "../Components/Scrollreveal"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShop } from '@fortawesome/free-solid-svg-icons'
 
 
 const Home = () => {
     return (
         <>
-        
+
             <div className="home-main">
                 <div className="home-head-img">
                     <ScrollReveal>
@@ -26,53 +23,57 @@ const Home = () => {
                         <img src={banner} alt="banner" className="home-banner-web" />
                     </ScrollReveal>
                     <ScrollReveal>
-                    <img src={banner2} alt="banner" className="home-banner-media" />
+                        <img src={banner2} alt="banner" className="home-banner-media" />
                     </ScrollReveal>
                 </div>
-                
+
                 <div className="home-products">
                     <ScrollReveal>
-                        <h2>Best Selling</h2>
+                        <h1 className="home-best-header">Best Selling</h1>
                     </ScrollReveal>
-                    
+
                     <div className="home-products-list">
                         <ScrollReveal>
-                        <div className="home-products-list-item">
-                            <img src={image1} alt="image1" />
-                            <h3>Incense Stick <br /> (தூபக்கட்டி)</h3>
-                            <p>A traditional aromatic incense stick used for daily pooja</p>
-                            <p>Starts From <strong>₹30</strong></p>
-                            <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
+                            <div className="home-products-list-item">
+                                <img src={image1} alt="image1" />
+                                <h3>Incense Stick <br /> (தூபக்கட்டி)</h3>
+                                <p>Starts From <strong>₹30</strong></p>
+                                <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
                             </div>
                         </ScrollReveal>
                         <ScrollReveal>
-                        <div className="home-products-list-item">
-                            <img src={image2} alt="image2" />
-                            <h3>Sambrani Cup <br />(சம்பிராணி கப்)</h3>
-                            <p>A divine resin cup used for daily pooja</p>
-                            <p>Starts From <strong>₹30</strong></p>
-                            <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
-                        </div>
+                            <div className="home-products-list-item">
+                                <img src={image2} alt="image2" />
+                                <h3>Sambrani Cup <br />(சம்பிராணி கப்)</h3>
+                                <p>Starts From <strong>₹30</strong></p>
+                                <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
+                            </div>
                         </ScrollReveal>
                         <ScrollReveal>
-                        <div className="home-products-list-item">
-                            <img src={image3} alt="image3" />
-                            <h3>Agarbathi Stick <br />(அகர்பத்தி)</h3>
-                            <p>A long-lasting fragrance stick</p>
-                            <p>Starts From <strong>₹30</strong></p>
-                            <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
-                        </div>
+                            <div className="home-products-list-item">
+                                <img src={image3} alt="image3" />
+                                <h3>Agarbathi Stick <br />(அகர்பத்தி)</h3>
+                                <p>Starts From <strong>₹30</strong></p>
+                                <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
+                            </div>
                         </ScrollReveal>
                         <ScrollReveal>
-                        <div className="home-products-list-item">
-                            <img src={image4} alt="image4" />
-                            <h3>Dhoop Cone <br />(தூப கோன்)</h3>
-                            <p>cone-shaped incense for pooja and meditation.</p>
-                            <p>Starts From <strong>₹30</strong></p>
-                            <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
-                        </div>
+                            <div className="home-products-list-item">
+                                <img src={image4} alt="image4" />
+                                <h3>Dhoop Cone <br />(தூப கோன்)</h3>
+                                <p>Starts From <strong>₹30</strong></p>
+                                <button><Link to="/products" className="home-btn-link">Buy Now</Link></button>
+                            </div>
                         </ScrollReveal>
+
                     </div>
+                    <ScrollReveal>
+                        <div className="home-products-shop-btn">
+                            <Link to="/products">
+                                <button>Shop Now <FontAwesomeIcon icon={faShop} /> </button>
+                            </Link>
+                        </div>
+                    </ScrollReveal>
                 </div>
             </div>
         </>
